fbb32007e1e8b21dee735530bd8b4980d109149f
Ammar Qammaz <ammarkov@gmail.com>
2010-09-05 02:16:55 +0300

    adding memory_hypervisor

---
+++ memory_hypervisor.cpp
@@ -0,0 +1,39 @@
+#include "memory_hypervisor.h"
+
+
+/* Pictures are malloced structures that carry RGB data on them */
+int LoadPicturesIfNeeded()
+{
+  return 0;
+}
+
+int UnLoadPicturesIfNeeded()
+{
+  return 0;
+}
+
+int ManagePicturesMemory()
+{
+   if ( UnLoadPicturesIfNeeded() == 0 ) { return 0; }
+   if ( LoadPicturesIfNeeded() == 0 ) { return 0; }
+   return 1;
+}
+
+
+/* Textures are GPU allocated structures that carry RGB data on them */
+int LoadTexturesIfNeeded()
+{
+  return 0;
+}
+
+int UnLoadTexturesIfNeeded()
+{
+  return 0;
+}
+
+int ManageTexturesMemory()
+{
+   if ( UnLoadTexturesIfNeeded() == 0 ) { return 0; }
+   if ( LoadTexturesIfNeeded() == 0 ) { return 0; }
+   return 1;
+}
---
+++ memory_hypervisor.h
@@ -0,0 +1,11 @@
+#ifndef MEMORY_HYPERVISOR_H_INCLUDED
+#define MEMORY_HYPERVISOR_H_INCLUDED
+
+int LoadPicturesIfNeeded();
+int UnLoadPicturesIfNeeded();
+int ManagePicturesMemory();
+int LoadTexturesIfNeeded();
+int UnLoadTexturesIfNeeded();
+int ManageTexturesMemory();
+
+#endif // MEMORY_HYPERVISOR_H_INCLUDED
