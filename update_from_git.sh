757bc9e3f05df998354023be1a14efc6bea79b6f
Ammar Qammaz <ammarkov@gmail.com>
2012-12-01 15:00:45 +0200

    ...

--- update_from_git.sh
+++ update_from_git.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+ # Since we want to update from git , this is the chance  to clone  a fresh verion of AmmarServer :) 
+# First lets see if we already got a cloned version 
   if [ -e src/AmmarServer/src/main.c ] 
    then
+      #We do , no need to clone everything just run the git pull commands and we are set..!
       cd src/AmmarServer
       ./update_from_git.sh
       cd ../../
-     else
-     echo "Could not find AmmarServer , cloning a fresh copy from github!"
+     else 
+     echo "Could not find AmmarServer , we have the stock NULL one only cloning a fresh copy from github!"
      cd src
+     rm -rf  src/AmmarServer
      git clone git://github.com/AmmarkoV/AmmarServer.git
     cd ..
    fi
